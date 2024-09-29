fc0b73f21d3721aadf7760b4c9fdd133df537f57
MisterMixler <milnik61@gmail.com>
2024-09-29 22:05:27 +0300

    added correct get

--- back/test_send.py
+++ back/test_send.py
@@ -1,25 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from flasgger import Swagger, swag_from
-import parse_exe
+import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
 
-# Mock database for demonstration purposes
-mock_db = {
-    "1": {
-        "PE_header": "PE header data for report 1",
-        "DOS_header": "DOS header data for report 1",
-        "other_field": "Other data"
-    },
-    "2": {
-        "PE_header": "PE header data for report 2",
-        "DOS_header": "DOS header data for report 2",
-        "other_field": "Other data"
-    }
-}
+def get_db_connection():
+    conn = sqlite3.connect('BD/files.db')
+    conn.row_factory = sqlite3.Row  # gets strings as a dict
+    return conn
 
 class Report(Resource):
     @swag_from({
@@ -28,8 +19,8 @@ class Report(Resource):
                 'description': 'Successful retrieval of report fields',
                 'examples': {
                     'application/json': {
-                        'PE_header': 'PE header data for report 1',
-                        'DOS_header': 'DOS header data for report 1'
+                        'header_first': {'example_key': 'example_value'},
+                        'header_second': {'example_key': 'example_value'}
                     }
                 }
             },
@@ -39,8 +30,8 @@ class Report(Resource):
         },
         'parameters': [
             {
-                'name': 'report_id',
-                'description': 'ID of the report',
+                'name': 'file_id',
+                'description': 'file_id to get fields',
                 'in': 'path',
                 'type': 'string',
                 'required': True
@@ -57,22 +48,21 @@ class Report(Resource):
             }
         ]
     })
-    def get(self, report_id):
+    def get(self, file_id):
         """Retrieve specified fields from a report."""
         required_fields = request.args.getlist('required_fields')
         
-        # Fetch the report from the mock database
-        report = mock_db.get(report_id)
-        
-        if not report:
+        conn = get_db_connection()
+        report = conn.execute('SELECT * FROM files WHERE id = ?', (file_id,)).fetchone()
+        conn.close()
+
+        if report is None:
             return {'message': 'Report not found'}, 404
-        
-        # Filter the report to return only the requested fields
-        result = {field: report[field] for field in required_fields if field in report}
-        
+        result = {field: report[field] for field in required_fields if field in report.keys()}
+
         return jsonify(result)
 
-api.add_resource(Report, '/report/<string:report_id>')
+api.add_resource(Report, '/report/<string:file_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
