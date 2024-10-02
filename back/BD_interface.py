0bf77e36bd74d15de9ad4505befe655bf7b7bca0
Mixler <MisterMixler>
2024-10-02 16:56:05 +0300

    adding users

--- back/BD_interface.py
+++ back/BD_interface.py
@@ -2,8 +2,8 @@ import sqlite3
 import json
 import magic
 import logging
-import parse_exe
-import parse_elf
+from back import parse_exe
+from back import parse_elf
 
 BD_path = 'BD/files.db'
 
@@ -22,51 +22,69 @@ class BD_int():
             format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-    def __insert_file(cursor, conn, file_type, header_first, header_second, import_table, export_table):
-        cursor.execute('''
-            INSERT INTO files (file_type, header_first, header_second, import_table, export_table)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (file_type, json.dumps(header_first),json.dumps(header_second), json.dumps(import_table), json.dumps(export_table)))
-        conn.commit()
-        generated_id = cursor.lastrowid
-        logging.info(f"File with id:{generated_id} added")
+    def __insert_file(self,username, file_type, header_first, header_second, import_table, export_table):
+        self.cursor.execute('''
+            INSERT INTO files (username, filetype, header_first, header_second, import_table, export_table)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (username, file_type, json.dumps(header_first),json.dumps(header_second), json.dumps(import_table), json.dumps(export_table)))
+        self.conn.commit()
+        generated_id = self.cursor.lastrowid
+        logging.info(f"[SUCCESS] File with id:{generated_id} added")
         return generated_id
 
-    def __insert_exe(cursor, conn, file_path):
+    def __insert_exe(self,username, file_path):
         parsed_ms_dos, parsed_pe_header, parsed_exports, parsed_imports = parse_exe.parse(file_path)
-        file_id = BD_int.__insert_file(cursor, conn, 'exe',
-                            header_first=parsed_ms_dos,
-                            header_second=parsed_pe_header,
-                            import_table=parsed_imports,
-                            export_table=parsed_exports)
+        file_id = self.__insert_file(username=username,
+                                     file_type='exe',
+                                     header_first=parsed_ms_dos,
+                                     header_second=parsed_pe_header,
+                                     import_table=parsed_imports,
+                                     export_table=parsed_exports)
         return file_id
 
-    def __insert_elf(cursor, conn, file_path):
+    def __insert_elf(self,username, file_path):
         header,segments,sections,symbols = parse_elf.parse_elf_header(file_path)
-        file_id = BD_int.__insert_file(cursor, conn, 'exe',
-                            header_first=header,
-                            header_second=segments,
-                            import_table=sections,
-                            export_table=symbols)
+        file_id = self.__insert_file(username=username, 
+                                     file_type='exe',
+                                     header_first=header,
+                                     header_second=segments,
+                                     import_table=sections,
+                                     export_table=symbols)
         return file_id
-
     
+    def user_exists(self, username):
+        self.cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        result = self.cursor.fetchone()
+        return result is not None
+    
+    def add_user(self, username, email, pwd_hash):
+        try:
+            if not self.user_exists(username):
+                self.cursor.execute("""
+                                    INSERT INTO users (username, is_admin, is_blocked, email, pwd_hash) 
+                                    VALUES (?, 0, 0, ?, ?)
+                                    """, (username, email, pwd_hash))
+                self.conn.commit
+                logging.info(f"[SUCCESS] user {username} was added to users table")
+            else:
+                return "user_exists" 
+        except Exception as e:
+            logging.info(e)
 
-    def add_file(file_path):
-        
+    def add_file(self,username, file_path):
         try:
             file_type = get_file_type(file_path)
-            if file_type != 'application/x-dosexec' and file_type != 'application/x-executable':
+            if 'exec' not in file_type:
                 raise ValueError(f"incorrect filetype:{file_path} - type:{file_type}")
             
-            if file_type == 'application/x-dosexec':
-                file_id = BD_int.__insert_exe(file_path)
+            if 'application' in file_type and ('portable-executable' in file_type or 'x-dosexec' in file_type):
+                file_id = self.__insert_exe(username, file_path)
             elif file_type == 'application/x-executable':
-                file_id = BD_int.__insert_elf(file_path)
+                file_id = self.__insert_elf(file_path)
             return file_id
         except Exception as e:
             logging.info(e)
             return -1
+        
     def __del__(self):
-        print("del")
         self.conn.close()
