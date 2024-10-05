cd8847e6f6864ae742bea8f66e386a7fac73cd9a
Grizzly <punxrules666@gmail.com>
2024-10-05 18:14:06 +0300

    report page

--- app.py
+++ app.py
@@ -174,10 +174,26 @@ def history():
 def show_report(file_id):
     bd = BD_int()
     report = bd.get_report(file_id)
-    return report
+    with open("file_content.txt", "r") as fp:
+        hex_ = fp.read()
+    formatted_hex = format_hex(hex_)
+    return render_template('report_new.html', report=report, formatted_hex=formatted_hex)
     # report = get_report(current_user.username, report_id)
     # return render_template('report.html', report=report)
 
+
+def format_hex(hex_str):
+    hex_lines = []
+    bytes_per_line = 16
+
+    for i in range(0, len(hex_str), bytes_per_line * 2):
+        offset = f"{i // 2:08X}"
+        hex_bytes = [hex_str[j:j+2] for j in range(i, min(i + bytes_per_line * 2, len(hex_str)), 2)]
+        hex_section = ' '.join(hex_bytes)
+        ascii_section = ''.join([chr(int(b, 16)) if 32 <= int(b, 16) <= 126 else '.' for b in hex_bytes])
+        hex_lines.append(f"{offset}  {hex_section:<47}  {ascii_section}")
+
+    return hex_lines
 
 # Download binary from report
 @app.route('/uploads/<string:username>/<string:filename>')
