1f26ddb0f0f38cd7369c41803c85c30f5d703796
SokoSasha <asokolova19@gmail.com>
2024-10-06 17:55:54 +0300

    Google tracking

--- evil_twin/mail/evil_sender.py
+++ evil_twin/mail/evil_sender.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup as bs
 
 
 def send_mail(TO, msg):
-    password = "trwfdogydawefhff"
+    password = "bolgragtofyqnmho"
     email = "exe.dumper@yandex.ru"
 
     with smtplib.SMTP_SSL("smtp.yandex.ru") as server:
@@ -26,9 +26,13 @@ def create_mail(rec, sen, subj, path):
     msg["To"] = rec
     msg["Subject"] = subj
 
+    google_pixel_id = "G-V1FEE03L5L"
+
+    user, mail_domain = rec.split("@")
+
     html = open(path).read()
+    html = html.replace("utm_medium=email", f"utm_medium={mail_domain}")
     html = bs(html, "html.parser")
-    html.prettify(formatter="html")
 
     with open("styles.css", "r") as css_file:
         styles = css_file.read()
@@ -37,11 +41,19 @@ def create_mail(rec, sen, subj, path):
     style_tag.string = styles
     html.head.append(style_tag)
 
+    subj_formatted = subj.replace(" ", "%")
+    tracking_pixel_url = f"https://www.google-analytics.com/collect?v=1&tid={google_pixel_id}&cid={user}&t=event&ec=emails&ea=open&dt={subj_formatted}"
+    tracking_pixel = f'<img src="{tracking_pixel_url}">'
+
+    html.body.append(bs(tracking_pixel, "html.parser"))
+
+    html.prettify(formatter="html")
+
     now = datetime.now() - timedelta(hours=3)
     if path == 'evil_mail.html':
         if now.time() > datetime.strptime("03:00", "%H:%M").time():
             now += timedelta(days=1)
-        now = now.strftime("%-d %B")
+        now = now.strftime("%-d %b")
     elif path == 'evil_mail_proto.html':
         now -= timedelta(hours=1, minutes=13)
         now = now.strftime("%-d %b, %H:%M")
@@ -58,9 +70,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evil mail sender")
     parser.add_argument('-r', '--recipient', type=str)
     parser.add_argument('-s', '--sender', type=str)
-    parser.add_argument('-t', '--subject', type=str)
+    parser.add_argument('-t', '--subject', type=str, default="")
     parser.add_argument('-f', '--html_file_path', type=str, choices=['evil_mail.html', 'evil_mail_proto.html'])
 
     args = parser.parse_args()
     mess = create_mail(args.recipient, args.sender, args.subject, args.html_file_path)
     send_mail(args.recipient, mess)
+
