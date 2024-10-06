99105789a398cacce09400fae6deddb8104ca9dd
Grizzly <punxrules666@gmail.com>
2024-10-06 15:55:11 +0300

    get_report returns dict with universal keys for every file type

--- logic/frontend.py
+++ logic/frontend.py
@@ -5,7 +5,13 @@ Format data for frontend
 from back.BD_interface import BD_int
 
 
-def get_report(file_id):
+def get_report_info(file_id):
     bd = BD_int()
     report = bd.get_report(file_id)
+
+    # if report["filetype"] == "exe":
+    #     json_fields = ['MS_DOS_header', 'PE_header', 'Import_table', 'Export_table']
+    # else:
+    #     json_fields = ['ELF_header', 'Segments', 'Sections', 'Symbols']
+    # for
     return report
