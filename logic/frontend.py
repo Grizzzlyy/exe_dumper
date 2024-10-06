83f24aea37a9c29ba0230ba4ddd13a612c0fe4cb
Grizzly <punxrules666@gmail.com>
2024-10-06 23:15:45 +0300

    before improving dynamic chunk loading

--- logic/frontend.py
+++ logic/frontend.py
@@ -20,17 +20,16 @@ def get_report_info(file_id):
 
             report[k][elem] = new_elem
 
-    k = "import_table"
-    for elem in report[k]:
-        new_elem = report[k][elem]
+    for k in ["import_table", "export_table"]:
+        for dll in report[k]:
+            for idx, func in enumerate(report[k][dll]):
+                new_func = func
 
-        # Get offset and position
-        mask = 2 ** (4 * 9) - 16
-        new_elem["g_offset"] = f"{new_elem['offset'] & mask :09X}"
+                # Get offset and position
+                mask = 2 ** (4 * 9) - 16
+                new_func["g_name_offset"] = f"{new_func['name_offset'] & mask :09X}"
+                new_func['g_rva_offset_offset'] = f"{new_func['rva_offset_offset'] & mask :09X}"
 
-        report[k][elem] = new_elem
-
-
-
+                report[k][dll][idx] = new_func
 
     return report
