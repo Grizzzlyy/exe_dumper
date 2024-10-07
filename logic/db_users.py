4d3f5639fa86dee4b16b5cf27de43090c72d2507
MisterMixler <milnik61@gmail.com>
2024-10-07 20:26:46 +0300

    db_users changed

--- logic/db_users.py
+++ logic/db_users.py
@@ -2,29 +2,10 @@ from werkzeug.security import generate_password_hash
 from logic.utils import login_type
 from back.BD_interface import BD_int
 
-tmp_db = [
-    {"username": "alice", "email": "alice@mail.com", "pwd_hash": generate_password_hash('alice'),
-     "has_access": True, "is_admin": True},
-    {"username": "bob", "email": "bob@mail.com", "pwd_hash": generate_password_hash('bob'),
-     "has_access": True, "is_admin": False},
-    {"username": "clark", "email": "clark@mail.com", "pwd_hash": generate_password_hash('clark'),
-     "has_access": True, "is_admin": False},
-    {"username": "james", "email": "daroslav.skiba@yandex.ru", "pwd_hash": generate_password_hash('james'),
-     "has_access": True, "is_admin": False},
-]
-
-
 def get_user_info(login):
     worker = BD_int()
-    if login_type(login) == "username":
-        if worker.user_exists():
-            return worker.get_user_info()
-        return None
-    elif login_type(login) == "email":
-        for usr in tmp_db:
-            if usr["email"] == login:
-                return usr
-        return None
+    if worker.user_exists():
+        return worker.get_user_info()
     else:
         # ERROR
         raise ValueError(f"login type is {login_type(login)}")
