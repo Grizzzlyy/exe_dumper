9d025551a14bbf89aec94432eadd4a0efa3d3570
MisterMixler <milnik61@gmail.com>
2024-10-12 14:32:19 +0300

    some fix

--- back/BD_interface.py
+++ back/BD_interface.py
@@ -98,7 +98,7 @@ class BD_int():
             if 'application' in file_type and ('portable-executable' in file_type or 'x-dosexec' in file_type):
                 file_id = self.__insert_exe(username, file_path)
             elif 'application' in file_type and 'exec' in file_type:
-                file_id = self.__insert_elf(file_path,file_path)
+                file_id = self.__insert_elf(username,file_path)
             return file_id
         except Exception as e:
             logging.info(e)
