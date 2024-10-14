aa6e2881af5fa1fa79c597cbbaf41988926789cf
SokoSasha <asokolova19@gmail.com>
2024-10-14 20:18:53 +0000

    docker update

--- back/BD_interface.py
+++ back/BD_interface.py
@@ -13,6 +13,7 @@ from back import parse_exe
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
@@ -29,6 +30,7 @@ class BD_int():
     def __init__(self):
         self.conn = psycopg2.connect(
             host=DB_HOST,
+            port=DB_PORT,
             database=DB_NAME,
             user=DB_USER,
             password=DB_PASSWORD
